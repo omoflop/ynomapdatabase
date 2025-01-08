6a39e308c1dc22416ed1108e8c1a1854b74f5985
omoflop <29988852+omoflop@users.noreply.github.com>
2025-01-08 13:04:56 -0800

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -9,16 +9,7 @@
 // ==/UserScript==
 
 const minimapButtonHTML = '<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M4 12L20 12M12 4L12 20M6.2 20H17.8C18.9201 20 19.4802 20 19.908 19.782C20.2843 19.5903 20.5903 19.2843 20.782 18.908C21 18.4802 21 17.9201 21 16.8V7.2C21 6.0799 21 5.51984 20.782 5.09202C20.5903 4.71569 20.2843 4.40973 19.908 4.21799C19.4802 4 18.9201 4 17.8 4H6.2C5.0799 4 4.51984 4 4.09202 4.21799C3.71569 4.40973 3.40973 4.71569 3.21799 5.09202C3 5.51984 3 6.07989 3 7.2V16.8C3 17.9201 3 18.4802 3.21799 18.908C3.40973 19.2843 3.71569 19.5903 4.09202 19.782C4.51984 20 5.07989 20 6.2 20Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>'
-
 const updatesPerSecond = 60
-
-function getMapId() {
-  return cachedMapId
-}
-
-function getPlayerPos() {
-  return easyrpgPlayer.api.getPlayerCoords()
-}
 
 const minimapToggleButton = document.createElement("button");
 minimapToggleButton.classList.add("iconButton");
@@ -26,7 +17,12 @@ minimapToggleButton.innerHTML = minimapButtonHTML;
 minimapToggleButton.style.display = 'none'
 
 minimapToggleButton.onclick = function() {
-  mapCanvas.style.display = mapCanvas.style.display === 'none' ? '' : 'none';
+  mapForceHidden = !mapForceHidden;
+
+  if (mapForceHidden) {
+      document.getElementById("locationLabel").textContent = `Location:`;
+  }
+
 }
 
 document.getElementById("rightControls").insertBefore(minimapToggleButton, document.getElementById("controls-fullscreen"));
@@ -39,6 +35,9 @@ mapCanvas.style.marginBottom = '10px';
 mapCanvas.style.userSelect = 'none';
 mapCanvas.style.display = 'none'
 
+// Insert minimap above chat
+document.getElementById("chatbox").insertBefore(mapCanvas, document.getElementById("chatboxContent"))
+
 var mapMouseDown = false;
 var mouseOffsetX = 0;
 var mouseOffsetY = 0;
@@ -48,6 +47,26 @@ var zoom = 1;
 
 var canvasMouseX = 0;
 var canvasMouseY = 0;
+
+const ctx = mapCanvas.getContext("2d");
+ctx.imageSmoothingEnabled = false;
+var previousMap = null
+
+var mapImage = undefined;
+var mapImageReady = false
+
+const assetServerAddress = "https://raw.githubusercontent.com/omoflop/ynomapdatabase/refs/heads/main/maps";
+const assetServerAddressSuffix = "?not-from-cache-please";
+var mapLoopType = "none"
+var mapTeleports = [];
+
+var px = 0
+var py = 0
+var lockedOnPlayer = true;
+var mapForceHidden = true;
+
+var pingX = undefined;
+var pingY = undefined;
 
 const pings = [];
 
@@ -61,27 +80,21 @@ function addPing(mapId, x, y, expireAt) {
 }
 
 mapCanvas.addEventListener("wheel", function(event) {
-  var isZoomingIn = event.deltaY < 0; // Note: Changed sign as negative deltaY means zoom in
+  var isZoomingIn = event.deltaY < 0;
   var rect = mapCanvas.getBoundingClientRect()
-  var mouseX = event.offsetX * mapCanvas.width / rect.width;  // Remove imagePanX - we want raw canvas coordinates
-  var mouseY = event.offsetY * mapCanvas.height / rect.height;  // Remove imagePanY
-  var zoomFactor = isZoomingIn ? 2 : 0.5;  // Reversed factors to match zoom direction
+  var mouseX = event.offsetX * mapCanvas.width / rect.width;
+  var mouseY = event.offsetY * mapCanvas.height / rect.height;
+  var zoomFactor = isZoomingIn ? 2 : 0.5;
 
-  // Convert mouse position to world space before zoom
   var worldMouseX = (mouseX - imagePanX) / zoom;
   var worldMouseY = (mouseY - imagePanY) / zoom;
 
-  // Apply zoom
   zoom *= zoomFactor;
 
-  // Calculate new pan position to keep mouse point fixed
   imagePanX = mouseX - worldMouseX * zoom;
   imagePanY = mouseY - worldMouseY * zoom;
   event.preventDefault();
 });
-
-var pingX = undefined;
-var pingY = undefined;
 
 mapCanvas.addEventListener("mousedown", function(event) {
   if (event.button == 0) {
@@ -105,7 +118,6 @@ mapCanvas.addEventListener("contextmenu", function(event) {
   event.preventDefault();
 });
 
-
 document.addEventListener("mouseup", function(event) {
    mapMouseDown = false;
 });
@@ -124,22 +136,15 @@ document.addEventListener("mousemove", function(event) {
 });
 
 
-// Insert minimap above chat
-document.getElementById("chatbox").insertBefore(mapCanvas, document.getElementById("chatboxContent"))
-
-const ctx = mapCanvas.getContext("2d");
-ctx.imageSmoothingEnabled = false;
-var previousMap = null
-
-var mapImage = undefined;
-var mapImageReady = false
-
 function update() {
   try {
     let mapId = getMapId()
     if (mapId != null) {
       let temp = getPlayerPos()
-      updateMap(mapId, temp[0], temp[1])
+      updateMap(mapId, temp[0], temp[1]);
+      if (!mapForceHidden) {
+        document.getElementById("locationLabel").textContent = `Location: (${mapId}, ${temp[0]}, ${temp[1]})`;
+      }
     }
   } catch (e) {
     console.log("Not in game yet", e)
@@ -147,19 +152,6 @@ function update() {
 
   }
   setTimeout(update, 1000 / updatesPerSecond)
-}
-
-let px = 0
-let py = 0
-let lockedOnPlayer = true;
-let mapForceHidden = true;
-
-function lerp(start, goal, delta) {
-  return start + (goal - start) * delta;
-}
-
-function approach(val, goal, step) {
-  return val < goal ? Math.min(goal, val + step) : Math.max(goal, val - step);
 }
 
 function centerOnPlayer() {
@@ -172,19 +164,16 @@ function centerOnPlayer() {
     imagePanY = centerY - (py + 8) * zoom;
 }
 
-function dist(x1, x2, y1, y2) {
-    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-}
-
 function updateMap(mapId, playerX, playerY) {
   if (previousMap != mapId) {
     console.log(`New Map Loaded: ${mapId} (prev ${previousMap})`)
-    loadMapImage(mapId)
+    loadMapImage(mapId);
     px = playerX*16;
     py = playerY*16;
   }
   previousMap = mapId;
 
+  mapCanvas.style.display = mapForceHidden || !mapImageReady ? 'none' : '';
 
   // Update player position if they're too far
   if (dist(playerX*16, px, playerY*16, py) > 16 * 4) {
@@ -277,7 +266,6 @@ function updateMap(mapId, playerX, playerY) {
       }
     }
 
-
     if (pingExists) {
         ctx.strokeStyle = "white";
         ctx.beginPath();
@@ -298,26 +286,13 @@ function updateMap(mapId, playerX, playerY) {
   }
 }
 
-function clamp(a, min, max) {
-  if (a > max) return max;
-  if (a < min) return min;
-  return a;
-}
-
-setTimeout(update, 1000 / updatesPerSecond)
-
-const assetServerAddress = "https://raw.githubusercontent.com/omoflop/ynomapdatabase/refs/heads/main/maps";
-const assetServerAddressSuffix = "?not-from-cache-please";
-var mapLoopType = "none"
-var mapTeleports = [];
-
 function loadMapImage(mapId) {
   const mapImageUrl = `${assetServerAddress}/${gameId}/${mapId}/map.png${assetServerAddressSuffix}`;
   const mapMetaUrl = `${assetServerAddress}/${gameId}/${mapId}/metadata.json${assetServerAddressSuffix}`;
 
   mapImageReady = false;
   mapImage = new Image();
-  mapImage.crossOrigin = 'anonymous'; // Add this if loading from a different domain
+  mapImage.crossOrigin = 'anonymous';
   mapImage.onload = function() {
       mapImageReady = true;
       if (!mapForceHidden) {
@@ -331,7 +306,6 @@ function loadMapImage(mapId) {
     console.log(`Failed to load map from url: ${mapImageUrl}`)
   }
   mapImage.src = mapImageUrl;
-  document.getElementById("locationLabel").textContent = `Location: (${mapId})`;
 
    fetch(mapMetaUrl)
     .then(response => {
@@ -341,6 +315,8 @@ function loadMapImage(mapId) {
     .then(data => {
       mapLoopType = data.loop_type ?? "both"
       mapTeleports = data.teleport_data ?? []
+
+      // Attempt to use yno's api to get the location name of each teleport
       for (let i = 0; i < mapTeleports.length; i++) {
         let curTp = mapTeleports[i];
         let temp = getLocalizedMapLocationsHtml(gameId, curTp.destination_map_id, curTp.destination_map_id, curTp.destination_x, curTp.destination_y, '<br>')
@@ -352,3 +328,31 @@ function loadMapImage(mapId) {
       mapLoopType = "both"
     });
 }
+
+function clamp(a, min, max) {
+  if (a > max) return max;
+  if (a < min) return min;
+  return a;
+}
+
+function dist(x1, x2, y1, y2) {
+    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+}
+
+function lerp(start, goal, delta) {
+  return start + (goal - start) * delta;
+}
+
+function approach(val, goal, step) {
+  return val < goal ? Math.min(goal, val + step) : Math.max(goal, val - step);
+}
+
+function getMapId() {
+  return cachedMapId;
+}
+
+function getPlayerPos() {
+  return easyrpgPlayer.api.getPlayerCoords();
+}
+
+setTimeout(update, 1000 / updatesPerSecond);
